c137c70da8f33c432f7081422f8fbc33cc1a3ac1
K-osA <evbayooooo@gmail.com>
2022-02-02 16:12:32 +0900

    Cleanup Function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("Created!");
+    return () => console.log("Destroyed.."); // cleanup function
+  }, []);
+  return <h1>Hello!</h1>;
+}
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => console.log("i run only once"), []);
-  useEffect(() => console.log("i run when 'counter' changes"), [counter]);
-  useEffect(
-    () => console.log("i run when 'keyword' changes", keyword),
-    [keyword]
-  );
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
